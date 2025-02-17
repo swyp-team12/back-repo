dd3ab0cd28088d78de3a88cbe80d0adb84f58f12
kkh <keeff1@naver.com>
2025-02-17 18:47:14 +0900

    Initial commit

---
+++ src/main/java/Swyp8/Team12/Team12Application.java
@@ -0,0 +1,13 @@
+package Swyp8.Team12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team12Application.class, args);
+	}
+
+}
---
+++ src/test/java/Swyp8/Team12/Team12ApplicationTests.java
@@ -0,0 +1,13 @@
+package Swyp8.Team12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
